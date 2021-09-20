1bea60f3d4487b360bd54066ebbc3b435a0443b5
ABrar Jahin <ajahin@iu.edu>
2021-09-20 11:31:57 -0400

    RNN model create trying

--- Rnn.py
+++ Rnn.py
@@ -28,16 +28,42 @@ class Rnn(object):
 	#Lambda Functions - End
 
 	def __init__(self, X_tr, y_tr, X_test, y_test, number_of_trees=500, model_filename = 'Rnn.sav'):
+		embedding_column_count = number_of_trees
+
 		self.X_tr, self.y_tr, self.X_test, self.y_test = X_tr, y_tr, X_test, y_test
 		self.model_filename = "../model/" + model_filename
 		#Define The Model
 		self.model = tf.keras.Sequential([
-			tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
-			tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(128, return_sequences=True)),
-			tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)),
-			tf.keras.layers.Dense(32, activation='relu'),
-			tf.keras.layers.Dense(1, activation='sigmoid')
+			#tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
+			tf.keras.layers.Embedding(
+					input_dim = self.X_tr.shape[0],
+					output_dim = embedding_column_count,
+					input_length=self.X_tr.shape[1]
+				),
+			#tf.keras.layers.Dropout(0.1),
+			tf.keras.layers.Bidirectional(
+					tf.keras.layers.LSTM(
+							128,
+							return_sequences=True
+						)
+				),
+			tf.keras.layers.Bidirectional(
+					tf.keras.layers.LSTM(64)
+				),
+			tf.keras.layers.Dense(
+					32,
+					activation='relu'
+				),
+			tf.keras.layers.Dense(
+					1,
+					activation='sigmoid'
+				)
 		])
+		self.logger = keras.callbacks.TensorBoard(
+			log_dir='logs',
+			write_graph=True,
+			histogram_freq=5
+		)
 		# Compile the model
 		self.model.compile(
 				loss='binary_crossentropy',
@@ -48,11 +74,19 @@ class Rnn(object):
 
 	def trainAndSaveModel(self):
 		# Fit the RNN - Training
-		history = self.model.fit(self.X_tr, self.y_tr['labels'], 
-                    batch_size=32,
-					epochs=10,
-                    validation_data=(self.X_test, self.y_test))
+		history = self.model.fit(
+						self.X_tr,
+						self.y_tr['labels'], 
+						batch_size=32,
+						epochs=10,
+						validation_data=(self.X_test, self.y_test),
+						shuffle=True,
+						verbose=2,
+						callbacks=[self.logger]
+					)
 		print(history)
+		test_error_rate = self.model.evaluate(self.X_test, self.y_test, verbose=1)
+		print("The mean squared error (MSE) for the test data set is: {}".format(test_error_rate))
 		#Save the model
 		pickle.dump(self.model, open(Utils.getAbsFilePath(self.model_filename), 'wb'))	#Store Model to File
 		return
